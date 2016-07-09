b211c5c9365b56e74717ccb75065462716a88855
vishwaspatil <vsp@narola.email>
2016-07-09 13:12:29 +0530

    asignment-2:product price,product attributes done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+	validates_presence_of :title , :message => "can't be blank"
+	validates_presence_of :description, :message => "can't be blank"
+	validates_presence_of :image_url, :message => "can't be blank"
+	validates_presence_of :price, :message => "can't be blank"	
+	validates_numericality_of :price, :greater_than => 0.01, :message => "can be greater_than or equal_to 0.01"
+	validates :title, :uniqueness => {:message => "Title already exists."}
+	validates :image_url, format: {with: /\.(png|jpg)\Z/i}
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,49 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+	test "should have the neccassary attribute " do
+		  product=Product.new(title: 'vishwas' , description: 'vishwas patil' , image_url: ''  , price: '')
+		  assert_not  product.valid?
+		  assert_equal [:title , :description , :image_url , :price], product.errors.full_messages
+	end
+	test "product price must be positive " do
+		  product=Product.new(title: 'vishwas123' , description: 'vishwas patil323' , image_url: 'vishwas.jpg'  , price: '-1')
+		  assert_not  product.valid?
+		  assert_equal [:title , :description , :image_url , :price], product.errors.full_messages
+	end
+
+	
+	def new_product(image_url)
+  		Product.new(title: "My Book Title",
+               		description: "yyy",
+               		price: 1,
+               		image_url:   image_url)
+	end
+
+	test "image url" do
+  		ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+        	 http://a.b.c/x/y/z/fred.gif }
+  		bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+  		ok.each do |name|
+    	assert new_product(name).valid?, "#{name} shouldn't be invalid"
+  		end
+
+  		bad.each do |name|
+    	assert new_product(name).invalid?, "#{name} shouldn't be valid"
+  		end
+	end
+
+
+	test "product is not valid without a unique title - i18n" do
+    product = Product.new( title:        products(:value1).title,
+                       description:  "yyy", 
+                       price: 1, 
+                       image_url: "fred.gif")
+
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),
+                 product.errors[:title].join('; ')
+             end
 end
