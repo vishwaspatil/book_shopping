760ac41a00c16a86b71f95a0aab5515c88afff60
vishwaspatil <vsp@narola.email>
2016-07-11 15:32:32 +0530

    validation code changed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,9 @@
 class Product < ActiveRecord::Base
-	validates_presence_of :title , :message => "can't be blank"
-	validates_presence_of :description, :message => "can't be blank"
-	validates_presence_of :image_url, :message => "can't be blank"
-	validates_presence_of :price, :message => "can't be blank"	
-	validates_numericality_of :price, :greater_than => 0.01, :message => "can be greater_than or equal_to 0.01"
-	validates :title, :uniqueness => {:message => "Title already exists."}
-	validates :image_url, format: {with: /\.(png|jpg)\Z/i}
 
-	scope :sorted, lambda { order("store.title ASC") }
+	validates :title, :uniqueness => {:message =>  " already exsits"} , presence: true
+    validates :description , presence: true
+    validates :image_url , presence: true , format: {with: /\.(png|jpg)\Z/i}
+    validates :price, :numericality => { :greater_than => 0.01  } 
+	
 
 end
